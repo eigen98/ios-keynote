6332e2d1d0341264d3bebbc57a31a96c3d229d88
eigen98 <terrantrace@gmail.com>
2023-07-18 23:33:16 +0900

    Feat : [3-1] 슬라이드 컴포넌트 타입 정의

--- MyKeynote/MyKeynote/Model/SlideElement.swift
+++ MyKeynote/MyKeynote/Model/SlideElement.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+protocol SlideElement {
+    var position: CGPoint { get set }
+    var size: CGSize { get set }
+    var animation: AnimationApplicable? { get set }
+    func display()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
