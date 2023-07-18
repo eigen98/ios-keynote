cce529d4dab6299ace931a78ee50190307ab64d1
eigen98 <terrantrace@gmail.com>
2023-07-18 23:31:04 +0900

    Feat : [3-1] 투명도를 나타내는 열거형 구현 (10단계)

--- MyKeynote/MyKeynote/Model/AlphaLevel.swift
+++ MyKeynote/MyKeynote/Model/AlphaLevel.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+enum AlphaLevel : Int{
+    case level1
+    case level2
+    case level3
+    case level4
+    case level5
+    case level6
+    case level7
+    case level8
+    case level9
+    case level10
+}
