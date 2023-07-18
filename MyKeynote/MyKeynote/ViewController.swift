bee07e7d7ade9a8a297bbb5913452cb6b2c3268f
eigen98 <terrantrace@gmail.com>
2023-07-18 23:35:29 +0900

    Feat : [3-1] Factory 인스턴스를 사용하여 4개의 슬라이드 생성 및 출력

--- MyKeynote/MyKeynote/ViewController.swift
+++ MyKeynote/MyKeynote/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
-
+import os.log
 class ViewController: UIViewController {
 
+    let factory = SlideFactory()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for i in 1...4{
+
+            let slide =  factory.createRandomSlide()
+            slide.alpha = AlphaLevel(rawValue: Int.random(in: 0...9)) ?? .level1
+            os_log("Rect\(i) \(slide.description)")
+            
+            
+        }
+        
+    
+
+        
+        
+       
+        
     }
 
 
