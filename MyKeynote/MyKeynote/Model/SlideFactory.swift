26bdb5f4e15e0a89a2b344fe5a1d0602f7d10e8f
eigen98 <terrantrace@gmail.com>
2023-07-18 23:32:38 +0900

    Feat : [3-1] 슬라이드 생성 팩토리 구현

--- MyKeynote/MyKeynote/Model/SlideFactory.swift
+++ MyKeynote/MyKeynote/Model/SlideFactory.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+protocol SlideFactoryProtocol{
+    func createSlide(length: Int, backgroundColor: SlideRGBColor) -> SquareSlide
+    func createRandomSlide() -> SquareSlide
+}
+
+class SlideFactory : SlideFactoryProtocol{
+    
+    
+    private var generator: RandomNumberGenerator
+    private var idGenerator: IDGenerator
+    
+    init(generator: RandomNumberGenerator = SystemRandomNumberGenerator()) {
+        self.generator = generator
+        self.idGenerator = IDGenerator()
+    }
+    func createSlide(length: Int, backgroundColor: SlideRGBColor) -> SquareSlide {
+         let id = idGenerator.generateID(using: &generator)
+
+         return SquareSlide(id: id, length: length, backgroundColor: backgroundColor)
+     }
+    
+    func createRandomSlide() -> SquareSlide {
+        let id = idGenerator.generateID(using: &generator)
+        let length = Int.random(in: 1...100, using: &generator)
+        let color = SlideRGBColor(
+            red: Int.random(in: 0...255, using: &generator),
+            green: Int.random(in: 0...255, using: &generator),
+            blue: Int.random(in: 0...255, using: &generator)
+        )
+        
+        return SquareSlide(id: id, length: length, backgroundColor: color)
+    }
+    
+}
+
