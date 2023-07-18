3a57729a5657f2e2801830afeb1bfdc749131e53
eigen98 <terrantrace@gmail.com>
2023-07-18 23:33:47 +0900

    Feat : [3-1] 정사각-슬라이드 뷰 모델 구현

--- MyKeynote/MyKeynote/Model/SquareSlide.swift
+++ MyKeynote/MyKeynote/Model/SquareSlide.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+class SquareSlide : Slide{
+    
+    private let id: String
+    var length: Int
+    var backgroundColor: SlideRGBColor
+    var alpha: AlphaLevel = .level1
+    private(set) var elements: [SlideElement] = []
+    
+    init(id: String, length: Int, backgroundColor: SlideRGBColor) {
+        self.id = id
+        self.length = length
+        self.backgroundColor = backgroundColor
+    }
+    
+    func prepareForDisplay() {
+        
+    }
+    
+    func addElement(_ element: SlideElement) {
+        self.elements.append(element)
+    }
+    
+    func removeElement(_ element: SlideElement) {
+        
+    }
+    
+    func display() {
+        
+    }
+    
+    
+}
+
+extension SquareSlide : CustomStringConvertible{
+    var description: String {
+        return "(\(id)), Side:\(length), R:\(backgroundColor.red), G:\(backgroundColor.green), B:\(backgroundColor.blue), Alpha: \(alpha.rawValue)"
+    }
+}
+
+
+
