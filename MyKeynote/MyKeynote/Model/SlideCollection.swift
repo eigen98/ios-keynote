410b78778d9389993c590b2914a9a6164e4c44d4
eigen98 <terrantrace@gmail.com>
2023-07-24 18:57:54 +0900

    Feat: (3-2) 슬라이드 배열 관리 객체 구현

--- MyKeynote/MyKeynote/Model/SlideCollection.swift
+++ MyKeynote/MyKeynote/Model/SlideCollection.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+struct SlideCollection {
+    private var slideArray : [BaseSlide] = []
+
+    subscript(index: Int) -> BaseSlide? {
+        return (index >= 0 && index < slideArray.count) ? slideArray[index] : nil
+    }
+    
+    mutating func addSlide(slide : BaseSlide){
+        slideArray.append(slide)
+        
+    }
+    
+    func getCount() -> Int{
+        return slideArray.count
+    }
+    
+}
