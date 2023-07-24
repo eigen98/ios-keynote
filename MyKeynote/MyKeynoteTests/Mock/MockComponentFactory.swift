c5698d083b3065e10e04350962178a926c32f86b
eigen98 <terrantrace@gmail.com>
2023-07-25 08:25:49 +0900

    Feat : (3-2) 컴포넌트 팩토리 모의 객체 구현

--- MyKeynote/MyKeynoteTests/Mock/MockComponentFactory.swift
+++ MyKeynote/MyKeynoteTests/Mock/MockComponentFactory.swift
@@ -5,4 +5,13 @@
 //  Created by KoJeongMin  on 2023/07/24.
 //
 
-import Foundation
+import XCTest
+@testable import MyKeynote
+class MockComponentFactory: SlideComponentFactoryProtocol {
+    var createComponentCalled = false
+    
+    func createComponent<Element>(length: Int, backgroundColor: SlideRGBColor, type: Element.Type) -> Element where Element : BaseElement {
+        createComponentCalled = true
+        return Element.init(id: "mock", position: .zero, size: .init(width: length, height: length))
+    }
+}
