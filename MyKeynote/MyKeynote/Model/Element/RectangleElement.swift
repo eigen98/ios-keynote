7bc4d6c7a148b664cb14990df646e86dd57dc5ec
eigen98 <terrantrace@gmail.com>
2023-07-24 18:54:28 +0900

    Feat: (3-2) 사각형 컴포넌트 객체 정의₩

--- MyKeynote/MyKeynote/Model/Element/RectangleElement.swift
+++ MyKeynote/MyKeynote/Model/Element/RectangleElement.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+class RectangleElement : BaseElement{
+    var width: CGFloat
+    var height: CGFloat
+    
+    init(id: String, position: CGPoint, size: CGSize, width: CGFloat, height: CGFloat) {
+        self.width = width
+        self.height = height
+        super.init(id: id, position: position, size: size)
+    }
+    
+    required init(id: String, position: CGPoint, size: CGSize) {
+        self.width = size.width
+        self.height = size.height
+        super.init(id: id, position: position, size: size)
+        
+    }
+}
