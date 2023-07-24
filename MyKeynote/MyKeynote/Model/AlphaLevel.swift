52889bf562ab992289c176fe5a571f5b77c03b60
eigen98 <terrantrace@gmail.com>
2023-07-24 18:51:58 +0900

    Feat : (3-2) 투명도 10단계 타입 열거형 정의

--- MyKeynote/MyKeynote/Model/AlphaLevel.swift
+++ MyKeynote/MyKeynote/Model/AlphaLevel.swift
@@ -6,15 +6,15 @@
 //
 
 import Foundation
-enum AlphaLevel : Int{
-    case level1
-    case level2
-    case level3
-    case level4
-    case level5
-    case level6
-    case level7
-    case level8
-    case level9
-    case level10
+enum AlphaLevel : Float{
+    case level1 = 0.1
+    case level2 = 0.2
+    case level3 = 0.3
+    case level4 = 0.4
+    case level5 = 0.5
+    case level6 = 0.6
+    case level7 = 0.7
+    case level8 = 0.8
+    case level9 = 0.9
+    case level10 = 1.0
 }
