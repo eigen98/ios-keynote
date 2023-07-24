7ad31c7030ada8c5f8cc107cd76cfa651ea96e94
eigen98 <terrantrace@gmail.com>
2023-07-24 18:56:41 +0900

    Feat : (3-2) 컴포넌트 팩토리 구현

--- MyKeynote/MyKeynote/Model/Factory/ComponentFactory.swift
+++ MyKeynote/MyKeynote/Model/Factory/ComponentFactory.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+protocol SlideComponentFactoryProtocol{
+    func createComponent<Element: BaseElement>(length: Int, backgroundColor: SlideRGBColor, type : Element.Type) -> Element
+}
+
+class SlideComponentFactory : SlideComponentFactoryProtocol{
+    
+    private var generator: RandomNumberGenerator
+    private var idGenerator: IDGenerator
+
+    init(generator: RandomNumberGenerator  = SystemRandomNumberGenerator()) {
+        self.generator = generator
+        self.idGenerator = IDGenerator()
+    }
+    
+    func createComponent<Element: BaseElement>( length: Int,
+                                                backgroundColor: SlideRGBColor,
+                                                type: Element.Type) -> Element where Element : SlideElementProtocol {
+        
+        return type.init(id: idGenerator.generateID(using: &generator),
+                         position: .zero,
+                         size: .init(width: length, height: length))
+    }
+    
+    
+    
+}
