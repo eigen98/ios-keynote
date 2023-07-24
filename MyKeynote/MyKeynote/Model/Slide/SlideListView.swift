22ef7d5ac6cad23e86b3fed02d30e9b0af572610
eigen98 <terrantrace@gmail.com>
2023-07-25 08:29:22 +0900

    Feat: (3-2) 앱의 첫 화면을 설정

---
+++ MyKeynote/MyKeynote/Model/Slide/SlideListView.swift
@@ -0,0 +1,23 @@
+//
+//  SlideListView.swift
+//  MyKeynote
+//
+//  Created by KoJeongMin  on 2023/07/19.
+//
+
+import Foundation
+import UIKit
+//좌측 뷰
+class SlideListView : UIView{
+    
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+}
