6b05b12ac8fa3d39f2b7c520dd0dc9eede8a0746
eigen98 <terrantrace@gmail.com>
2023-07-26 16:25:19 +0900

    Feat : (3-3) SlideRGBColor Extension 생성자 추가(UIColor를 인자로 생성)

--- MyKeynote/MyKeynote/Extension/SlideRGBColor+.swift
+++ MyKeynote/MyKeynote/Extension/SlideRGBColor+.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import UIKit
+extension SlideRGBColor {
+    init?(color: UIColor) {
+        guard let components = color.cgColor.components, components.count >= 3 else {
+            return nil
+        }
+        
+        self.red = Int(components[0] * 255.0)
+        self.green = Int(components[1] * 255.0)
+        self.blue = Int(components[2] * 255.0)
+    }
+}
