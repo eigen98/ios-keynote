2bb786c2da82746391bc03f22c037ccab2120da6
eigen98 <terrantrace@gmail.com>
2023-07-28 17:29:25 +0900

    Feat : (3-4) 컴포넌트 팩토리 랜덤 컴포넌트 생성 메소드 구현

--- MyKeynote/MyKeynote/Model/Factory/ComponentFactory.swift
+++ MyKeynote/MyKeynote/Model/Factory/ComponentFactory.swift
@@ -8,13 +8,14 @@
 import Foundation
 protocol SlideComponentFactoryProtocol{
     func createComponent<Element: BaseElement>(length: Int, backgroundColor: SlideRGBColor, type : Element.Type) -> Element
+    func createRandomComponent<Element: BaseElement>(type: Element.Type) -> Element
 }
 
 class SlideComponentFactory : SlideComponentFactoryProtocol{
     
     private var generator: RandomNumberGenerator
     private var idGenerator: IDGenerator
-
+    
     init(generator: RandomNumberGenerator  = SystemRandomNumberGenerator()) {
         self.generator = generator
         self.idGenerator = IDGenerator()
@@ -28,7 +29,12 @@ class SlideComponentFactory : SlideComponentFactoryProtocol{
                          position: .zero,
                          size: .init(width: length, height: length))
     }
-    
+    func createRandomComponent<Element: BaseElement>(type: Element.Type) -> Element where Element : SlideElementProtocol {
+        let length = Int.random(in: 0...500)
+        return type.init(id: idGenerator.generateID(using: &generator),
+                         position: .zero,
+                         size: .init(width: length, height: length))
+    }
     
     
 }
